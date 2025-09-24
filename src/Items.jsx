ea73c6852ba0e1d8636bb4227d3ac83807c1a5f0
Mupenzi Herve <mupenziherve04@gmail.com>
2025-09-24 22:55:58 +0200

    error handleling

--- src/Items.jsx
+++ src/Items.jsx
@@ -2,19 +2,29 @@ import { useQuery } from '@tanstack/react-query';
 import SingleItem from './SingleItem';
 import customFetch from './utils';
 const Items = ({ items }) => {
-  const {isLoading, data} = useQuery({
-    queryKey: ['tasks'], queryFn:() => customFetch.get('/'),
-  });
+  const {isLoading, data, isError, error} = useQuery({
+    queryKey: ['tasks'], queryFn:() => customFetch.get('/Something'),
+  }); 
   if (isLoading) {
-    return <p style={{margin:'1rem'}}>Loading please wait</p>
+     return <p style={{margin:'1rem'}}>Loading please wait...</p>
      }
+      // if (isError) {
+      //   return <p style={{ margin: '1rem' }}>Error encounter...</p>
+      // }
+      console.log(error);
+
+      if (error) {
+        return <p style={{ margin: '1rem' }}>{error.message}</p>
+      }
+
+
 
   return (
-    <div className="items">
+    <div className='items'>
       {data.data.taskList.map((item) => {
-        return <SingleItem key={item.id} item={item} />
+        return <SingleItem key={item.id} item={item} />;
       })}
     </div>
-  )
+  );
 };
 export default Items;
