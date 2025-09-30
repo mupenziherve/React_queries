152fa247cec4be98a33b29126508746ba0c9406d
Mupenzi Herve <mupenziherve04@gmail.com>
2025-09-30 23:00:15 +0200

    altenative

--- src/reactQueryCustomHook.jsx
+++ src/reactQueryCustomHook.jsx
@@ -1,10 +1,14 @@
-
+import {}
 
 export const useFetchTasks = () => {
 
     const { isLoading, data, isError, error } = useQuery({
       queryKey: ['tasks'],
       queryFn: () => customFetch.get('/'),
-    }) 
-    
+    }); 
+
+    return {isLoading, isError, data}
+    dbbbbbcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccslcnkksncvknsknvknskv
+    svdsdvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+    vvvvvvvvvvvvvvvvvvvvvvvvvv
 }
