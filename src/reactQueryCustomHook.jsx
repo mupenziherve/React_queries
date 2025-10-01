0c2700ed4fccbcffe468467eca44bbf69714defe
Mupenzi Herve <mupenziherve04@gmail.com>
2025-10-01 19:13:36 +0200

    update

--- src/reactQueryCustomHook.jsx
+++ src/reactQueryCustomHook.jsx
@@ -10,5 +10,3 @@ export const useFetchTasks = () => {
     return {isLoading, isError, data};
     
 }
-lllllllllllllllllllllllllllllllllllllLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
-LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
